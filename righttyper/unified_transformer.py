b3081bcf0edac320b4067bb2bbefd8523cc206c3
Juan Altmayer Pizzorno <juan@altmayer.com>
2024-10-08 14:06:32 -0400

    - switched to emitting imports based on type names, greatly simplifying   code and also avoiding numerous unnecessary (and possibly semantics   changing) imports;
    - various other small improvements;

--- righttyper/unified_transformer.py
+++ righttyper/unified_transformer.py
@@ -2,7 +2,6 @@ from typing import Dict, List, Optional, Set, Tuple
 
 import libcst as cst
 
-from righttyper.get_import_details import generate_import_nodes
 from righttyper.righttyper_types import (
     ArgumentName,
     Filename,
@@ -46,6 +45,17 @@ _TYPING_TYPES : Set[Typename] = {
 }
 
 
+def _dotted_name(name: str) -> cst.Attribute | cst.Name:
+    """Creates CST nodes equivalent to a module name, dotted or not"""
+    parts = name.split(".")
+    if len(parts) == 1:
+        return cst.Name(parts[0])
+    base: cst.Name | cst.Attribute = cst.Name(parts[0])
+    for part in parts[1:]:
+        base = cst.Attribute(value=base, attr=cst.Name(part))
+    return base
+
+
 class UnifiedTransformer(cst.CSTTransformer):
     def __init__(
         self,
@@ -58,27 +68,26 @@ class UnifiedTransformer(cst.CSTTransformer):
             ],
         ],
         not_annotated: Dict[FuncInfo, Set[ArgumentName]],
-        allowed_types: List[Typename] = [],
-        imports: Set[ImportInfo] = set(),
+        allowed_types: List[Typename] = [], # TODO delete?
+        imports: Set[ImportInfo] = set(), # TODO delete
     ) -> None:
         # Initialize AnnotateFunctionTransformer data
         self.filename = filename
         self.type_annotations = type_annotations
         self.not_annotated = not_annotated
-        self.allowed_types : Set[Typename] = set(allowed_types) | _BUILTIN_TYPES | _TYPING_TYPES
+        self.known_types : Set[Typename] = set(allowed_types) | _BUILTIN_TYPES | _TYPING_TYPES
 
-        # Initialize ConstructImportTransformer data
-        self.imports = imports
 
-        # Initialize InsertTypingImportTransformer data
-        self.has_typing_import = False
-
-        # Track `from __future__` imports
-        self.future_imports: List[cst.SimpleStatementLine] = []
-
-    # Helper method from AnnotateFunctionTransformer
     def _should_output_as_string(self, annotation: str) -> bool:
-        return not all(t in self.allowed_types for t in types_in_annotation(annotation))
+        return any(t not in self.known_types for t in types_in_annotation(annotation))
+
+
+    def visit_Module(self, node: cst.Module) -> bool:
+        # Initialize mutable members here, just in case transformer gets reused
+        self.used_types : Set[Typename] = set()
+        # TODO modify known_types based on existing imports, so that they're not unnecessarily imported
+        return True
+
 
     # AnnotateFunctionTransformer logic
     def leave_FunctionDef(
@@ -96,6 +105,8 @@ class UnifiedTransformer(cst.CSTTransformer):
                     if parameter.name.value == arg:
                         if arg not in self.not_annotated.get(key, set()):
                             continue
+
+                        # TODO recognize (and use) import aliases, transforming annotation
                         annotation_expr: cst.BaseExpression
                         if self._should_output_as_string(annotation_):
                             annotation_expr = cst.SimpleString(
@@ -111,23 +122,25 @@ class UnifiedTransformer(cst.CSTTransformer):
                                 )
                             )
                         )
+                        self.used_types |= types_in_annotation(annotation_)
                         break
                 else:
                     new_parameters.append(parameter)
 
-            return_type_expr: cst.BaseExpression
-            if self._should_output_as_string(return_type):
-                return_type_expr = cst.SimpleString(f'"{return_type}"')
-            else:
-                return_type_expr = cst.parse_expression(return_type)
-
             if "return" in self.not_annotated.get(key, set()):
+                return_type_expr: cst.BaseExpression
+                if self._should_output_as_string(return_type):
+                    return_type_expr = cst.SimpleString(f'"{return_type}"')
+                else:
+                    return_type_expr = cst.parse_expression(return_type)
+
                 updated_node = updated_node.with_changes(
                     params=updated_node.params.with_changes(
                         params=new_parameters
                     ),
                     returns=cst.Annotation(annotation=return_type_expr),
                 )
+                self.used_types |= types_in_annotation(return_type)
             else:
                 updated_node = updated_node.with_changes(
                     params=updated_node.params.with_changes(
@@ -140,7 +153,8 @@ class UnifiedTransformer(cst.CSTTransformer):
     def leave_Module(
         self, original_node: cst.Module, updated_node: cst.Module
     ) -> cst.Module:
-        # Step 1: Collect `from __future__` imports and remove them
+        # Collect `from __future__` imports and remove them
+        future_imports: List[cst.BaseStatement] = []
         new_body: List[cst.BaseStatement] = []
         stmt: cst.BaseStatement
         for stmt in updated_node.body:
@@ -153,34 +167,29 @@ class UnifiedTransformer(cst.CSTTransformer):
                     for imp in stmt.body
                 ):
                     # Collect future imports to lift later
-                    self.future_imports.append(stmt)
+                    future_imports.append(stmt)
                 else:
                     new_body.append(stmt)
             else:
                 new_body.append(stmt)
             updated_node = updated_node.with_changes(body=new_body)
 
-        # Add additional imports if needed
-        if self.imports:
-            new_imports = []
-            for imp in self.imports:
-                # Make sure not to include imports specific to righttyper
-                if "righttyper" not in imp.function_fname:
-                    new_imports.extend(
-                        generate_import_nodes(imp.import_details)
-                    )
-
-            valid_imports = [
-                imp
-                for imp in new_imports
-                if not isinstance(imp, cst.EmptyLine)
-            ]
-
+        # Add additional type checking imports if needed
+        unknown_types = self.used_types - self.known_types
+        if unknown_types:
+            # TODO update any existing "if TYPE_CHECKING"
+            unknown_modules = {
+                '.'.join(name.split('.')[:-1]) if '.' in name else name
+                for name in unknown_types
+            }
             type_checking_imports = cst.If(
                 test=cst.Name("TYPE_CHECKING"),
                 body=cst.IndentedBlock(
                     body=[
-                        cst.SimpleStatementLine([imp]) for imp in valid_imports
+                        cst.SimpleStatementLine([
+                            cst.Import([cst.ImportAlias(_dotted_name(m))])
+                        ])
+                        for m in sorted(unknown_modules)
                     ]
                 ),
             )
@@ -188,7 +197,9 @@ class UnifiedTransformer(cst.CSTTransformer):
             new_body = [type_checking_imports] + list(new_body)
             updated_node = updated_node.with_changes(body=new_body)
 
-        if not self.has_typing_import:
+        # Emit "from typing import ..."
+        if unknown_types or (self.used_types & _TYPING_TYPES):
+            # TODO update existing import statement, if any
             typing_import = cst.SimpleStatementLine(
                 body=[
                     cst.ImportFrom(
@@ -197,16 +208,15 @@ class UnifiedTransformer(cst.CSTTransformer):
                             cst.ImportAlias(name=cst.Name(value="TYPE_CHECKING"))
                         ] + [
                             cst.ImportAlias(name=cst.Name(value=t))
-                            for t in _TYPING_TYPES
+                            for t in (self.used_types & _TYPING_TYPES)
                         ]
                     )
                 ]
             )
             new_body = [typing_import] + list(updated_node.body)
             updated_node = updated_node.with_changes(body=new_body)
-            self.has_typing_import = True
 
-        # Step 2: Determine where to insert the `from __future__` imports
+        # Determine where to insert the `from __future__` imports
         insertion_index = 0
         for i, stmt in enumerate(new_body):
             if (
@@ -222,12 +232,11 @@ class UnifiedTransformer(cst.CSTTransformer):
         # Insert future imports at the top (after comments and whitespace)
         new_body = (
             new_body[:insertion_index]
-            + self.future_imports
+            + future_imports
             + new_body[insertion_index:]
         )
 
         updated_node = updated_node.with_changes(body=new_body)
-
         return updated_node
 
 
