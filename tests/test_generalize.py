1c0058d3bc5be8cc60e9f9908ccdbebb9c4f520b
Juan Altmayer Pizzorno <juan@altmayer.com>
2025-01-07 13:21:31 -0500

    - first working version, but needs DRYing;

--- tests/test_generalize.py
+++ tests/test_generalize.py
@@ -1,9 +1,13 @@
 from righttyper.righttyper_types import TypeInfo, TypeInfoSet
 from righttyper.righttyper_utils import union_typeset_str
 import pytest
+from typing import Iterator
 
 def ti(name: str, **kwargs) -> TypeInfo:
     return TypeInfo(module='', name=name, **kwargs)
+
+def get_source(ti: TypeInfo) -> str:
+    f"{ti.module}.{ti.name}" if ti.module else ti.name
 
 NoneTypeInfo = ti('None')
 
@@ -17,15 +21,50 @@ def generalize(samples: list[tuple[TypeInfo, ...]]) -> list[str]:
 
     transposed = list(zip(*samples))
 
-    for i, types in enumerate(transposed):
-        if types in typevars:
-            parameters.append(typevars[types])
-        else:
-            if types in transposed[i+1:]:
+    from collections import Counter
+
+    def expand_generics(types: tuple[TypeInfo, ...]) -> Iterator[tuple[TypeInfo, ...]]:
+        print(f"types={[str(t) for t in types]}")
+        yield types
+
+        first = types[0]
+        if first.args and all(
+            t.module == first.module and 
+            t.name == first.name and 
+            len(t.args) == len(first.args)
+            for t in types[1:]
+        ):
+            for i in range(len(first.args)):
+                yield from expand_generics(tuple(t.args[i] for t in types))
+
+    occurrences = Counter()
+    for types in transposed:
+        occurrences.update([s for s in expand_generics(types)])
+
+    def rebuild(types: tuple[TypeInfo, ...]) -> TypeInfo|str:
+        first = types[0]
+        if first.args and all(
+            t.module == first.module and 
+            t.name == first.name and 
+            len(t.args) == len(first.args)
+            for t in types[1:]
+        ):
+            args = []
+            for i in range(len(first.args)):
+                args.append(rebuild(tuple(t.args[i] for t in types)))
+
+            # FIXME replace with TypeInfo.with_changes()
+            return TypeInfo(module=first.module, name=first.name, args=tuple(args))
+
+        if occurrences[types] > 1:
+            if types not in typevars:
                 typevars[types] = f"T{len(typevars)+1}"
-                parameters.append(typevars[types])
-            else:
-                parameters.append(union_typeset_str(TypeInfoSet(types)))
+            return typevars[types]
+
+        return union_typeset_str(TypeInfoSet(types))
+
+    for types in transposed:
+        parameters.append(str(rebuild(types)))
     
     return parameters
 
@@ -59,59 +98,82 @@ def test_none():
 
 
 def test_uniform_single_type():
-    tuples = [
+    samples = [
         (int, int, int),
         (bool, bool, bool),
         (float, float, float)
     ]
-    assert generalize2(tuples) == ['T1', 'T1', 'T1']
+    assert generalize2(samples) == ['T1', 'T1', 'T1']
+
+def test_uniform_single_type_with_generic():
+    samples = [
+        (ti('int'), ti('int')),
+        (ti('bool'), ti('bool')),
+        (ti('X', args=(ti('foo'),)), ti('X', args=(ti('foo'),))),
+    ]
+    assert generalize(samples) == ['T1', 'T1']
 
 def test_first_same_then_different():
-    tuples = [
+    samples = [
         (int, int),
         (bool, bool),
         (int, bool)
     ]
-    assert generalize2(tuples) == ['bool|int', 'bool|int']
+    assert generalize2(samples) == ['bool|int', 'bool|int']
 
 
 def test_mixed_with_constant_types():
-    tuples = [
+    samples = [
         (int, str, int),
         (bool, str, float),
         (float, str, bool)
     ]
-    assert generalize2(tuples) == ['bool|float|int', 'str', 'bool|float|int']
+    assert generalize2(samples) == ['bool|float|int', 'str', 'bool|float|int']
 
 
 def test_shared_variability():
-    tuples = [
+    samples = [
         (int, int, bool, int),
         (float, float, bool, float)
     ]
-    assert generalize2(tuples) == ['T1', 'T1', 'bool', 'T1']
+    assert generalize2(samples) == ['T1', 'T1', 'bool', 'T1']
 
 
 def test_multiple_length_tuples():
-    tuples = [
+    samples = [
         (int, int),
         (int, int, int),
     ]
-    assert generalize2(tuples) is None
+    assert generalize2(samples) is None
 
 
 def test_all_distinct_types():
-    tuples = [
+    samples = [
         (int, str, float, bool),
         (float, str, bool, int)
     ]
-    assert generalize2(tuples) == ['float|int', 'str', 'bool|float', 'bool|int']
+    assert generalize2(samples) == ['float|int', 'str', 'bool|float', 'bool|int']
 
 
-@pytest.mark.xfail()
 def test_generic():
-    tuples = [
+    samples = [
         (ti('int'), ti('list', args=(ti('int'),))),
         (ti('float'), ti('list', args=(ti('float'),))),
     ]
-    assert generalize(tuples) == ['T1', 'list[T1]']
+    assert generalize(samples) == ['T1', 'list[T1]']
+
+
+def test_generic_among_options():
+    samples = [
+        (ti('int'), ti('list', args=(ti('int'),))),
+        (ti('X', args=(ti('foo'),)), ti('list', args=(ti('X', args=(ti('foo'),)),))),
+    ]
+    assert generalize(samples) == ['T1', 'list[T1]']
+
+
+def test_generic_within_args():
+    samples = [
+        (ti('tuple', args=(ti('int'),)), ti('list', args=(ti('int'),))),
+        (ti('tuple', args=(ti('float'),)), ti('list', args=(ti('float'),))),
+    ]
+    assert generalize(samples) == ['tuple[T1]', 'list[T1]']
