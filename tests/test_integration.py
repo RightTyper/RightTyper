8100b1d13356e3664ad391cf758b5c436b529fa0
Maxwell Tang <amoinus@gmail.com>
2025-01-10 11:36:13 -0500

    Added __len__ error tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1099,17 +1099,39 @@ def test_union_superclass(tmp_cwd, as_module):
     assert "def foo(x: A) -> None:" in Path("t.py").read_text()
 
 
-def test_custom_collection(tmp_cwd):
-    Path("t.py").write_text(textwrap.dedent("""\
-        import collections.abc
+@pytest.mark.parametrize('superclass', ["list", "set", "dict", "KeysView", "ValuesView", "ItemsView", "tuple"])
+def test_custom_collection_len_error(tmp_cwd, superclass):
+    Path("t.py").write_text(textwrap.dedent(f"""\
+        from collections.abc import *
 
-        class MyKeysView(collections.abc.KeysView):
+        class MyContainer({superclass}):
             def __init__(self):
                 super()
+            
+            def __len__(self):
+                raise Exception("Oops, something went wrong!")
 
-        my_object = MyKeysView()
+
+        def foo(bar):
+            pass
+        
+
+        my_object = MyContainer()
+        foo(my_object)
         """
     ))
 
     subprocess.run([sys.executable, '-m', 'righttyper', '--overwrite', '--output-files',
-                    '--no-use-multiprocessing', 't.py'], check=True)
+                    '--no-use-multiprocessing', '-m', 't'], check=True)
+
+    # Are we supposed to generate types in the __main__ module?
+    if superclass in ["list", "set"]:
+        assert f"def foo(bar: \"__main__.MyContainer[Never]\") -> None" in Path("t.py").read_text()
+    elif superclass in ["dict"]:
+        assert f"def foo(bar: \"__main__.MyContainer[Never, Never]\") -> None" in Path("t.py").read_text()
+    elif superclass in ["KeysView", "ValuesView"]:
+        assert f"def foo(bar: {superclass}[Never]) -> None" in Path("t.py").read_text()
+    elif superclass in ["ItemsView"]:
+        assert f"def foo(bar: {superclass}[Never, Never]) -> None" in Path("t.py").read_text()
+    elif superclass in ["tuple"]:
+        assert f"def foo(bar: tuple) -> None" in Path("t.py").read_text()
