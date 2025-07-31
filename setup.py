ea8b5386b51d32312fb884f4c6e50ab43a35cb4b
Juan Altmayer Pizzorno <juan@altmayer.com>
2025-07-31 15:57:59 -0400

    - changed to adjust README dynamically during packaging, so that   relative URLs can be used (and things still work in pypi).   Relative URLs are important to keeping images consistent with   versions, especially as the repo is anonymized, etc.;

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import setuptools
 import sys
 import os
+import re
 from pathlib import Path
 
 try:
@@ -19,7 +20,25 @@ def get_version():
     )
 
 
+def get_url():
+    return tomllib.loads(Path("pyproject.toml").read_text())['project']['urls']['Repository']
+
+
+def long_description():
+    text = Path("README.md").read_text(encoding="utf-8")
+
+    # Rewrite any relative paths to version-specific absolute paths,
+    # so that they work from within PyPI
+    sub = r'\1' + get_url() + "/blob/v" + get_version() + r'/\2'
+    text = re.sub(r'(src=")((?!https?://))', sub, text)
+    text = re.sub(r'(\[.*?\]\()((?!https?://))', sub, text)
+
+    return text
+
+
 setuptools.setup(
     packages=['righttyper'],
     version=get_version(),
+    long_description=long_description(),
+    long_description_content_type="text/markdown",
 )
