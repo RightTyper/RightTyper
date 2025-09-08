79c7df5f49ad6d7e5500dd5cd468729c208825ab
Juan Altmayer Pizzorno <juan@altmayer.com>
2025-09-08 09:17:10 -0400

    - modified plugin to also collect the set of test modules, to be   used in upcoming features;

--- righttyper/pytest.py
+++ righttyper/pytest.py
@@ -1,6 +1,7 @@
 import pytest
 
 pytest_modules: set[str] = set()
+pytest_files : set[str] = set()
 
 def pytest_collection_modifyitems(session, config, items):
     """Collects the names of pytest test modules."""
@@ -8,3 +9,5 @@ def pytest_collection_modifyitems(session, config, items):
     for item in items:
         if (mod := getattr(item, "module", None)):
             pytest_modules.add(mod.__name__)
+        if (path := getattr(item, "fspath", None)):
+            pytest_files.add(path)
