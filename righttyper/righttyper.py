0d52aae1997d7f77fb72fb683c45087760a87579
Juan Altmayer Pizzorno <juan@altmayer.com>
2025-11-07 10:22:38 -0500

    - factored various transformers out Observations, making it easier to   modify;

--- righttyper/righttyper.py
+++ righttyper/righttyper.py
@@ -24,7 +24,7 @@ import collections.abc as abc
 from collections import defaultdict, Counter
 from dataclasses import dataclass, field, asdict
 from pathlib import Path
-from types import CodeType, FrameType, FunctionType, MethodType, GeneratorType, AsyncGeneratorType, CoroutineType, UnionType
+from types import CodeType, FrameType, FunctionType, GeneratorType, AsyncGeneratorType
 import typing
 from typing import (
     Any,
@@ -91,6 +91,16 @@ import righttyper.loader as loader
 from righttyper.righttyper_alarm import (
     SignalAlarm,
     ThreadAlarm,
+)
+from righttyper.type_transformers import (
+    SelfT,
+    NeverSayNeverT,
+    NoReturnToNeverT,
+    ExcludeTestTypesT,
+    ResolveMocksT,
+    TypesUnionT,
+    DepthLimitT,
+    MakePickleableT
 )
 
 from righttyper.options import Options, options
@@ -216,42 +226,6 @@ def get_typeshed_arg_types(
             return pos_args + kw_args
 
     return None
-
-
-def resolve_mock(ti: TypeInfo, adjuster: AdjustTypeNamesT|None) -> TypeInfo|None:
-    """Attempts to map a test type, such as a mock, to a production one."""
-    import unittest.mock as mock
-
-    trace = [ti]
-    t = ti
-    while is_test_module(t.module):
-        if not t.type_obj:
-            return None
-
-        non_unittest_bases = [
-            b for b in t.type_obj.__bases__ if b not in (mock.Mock, mock.MagicMock)
-        ]
-
-        # To be conservative, we only recognize classes with a single base (besides any Mock).
-        # If the only base is 'object', we couldn't find a non-mock module base.
-        if len(non_unittest_bases) != 1 or (base := non_unittest_bases[0]) is object:
-            return None
-
-        t = get_type_name(base)
-        if adjuster:
-            t = adjuster.visit(t)
-        trace.append(t)
-        if len(trace) > 50:
-            return None # break loops
-
-    if t is ti:
-        # 'ti' didn't need resolution.  Indicate no need to replace the type.
-        return None
-
-    if logger.level == logging.DEBUG:
-        logger.debug(f"Resolved mock {' -> '.join([str(t) for t in trace])}")
-
-    return t
 
 
 @dataclass
@@ -695,143 +669,35 @@ class Observations:
 
         self._transform_types(PostponedIteratorArgsT())
 
-        class SelfT(TypeInfo.Transformer):
-            """Renames types to typing.Self according to is_self."""
-            def visit(vself, node: TypeInfo) -> TypeInfo:
-                if node.is_self:
-                    return TypeInfo("typing", "Self")
-
-                return super().visit(node)
-
         if options.use_typing_self:
             self._transform_types(SelfT())
-
-        class NeverSayNeverT(TypeInfo.Transformer):
-            """Removes uses of typing.Never, replacing them with typing.Any"""
-            def visit(vself, node: TypeInfo) -> TypeInfo:
-                if node.type_obj is typing.Never:
-                    return AnyTypeInfo
-
-                return super().visit(node)
-
-        class NoReturnIsNowNeverT(TypeInfo.Transformer):
-            """Converts typing.NoReturn to typing.Never,
-               which is the more modern way to type a 'no return'"""
-            def visit(vself, node: TypeInfo) -> TypeInfo:
-                if node.type_obj is typing.NoReturn:
-                    return TypeInfo.from_type(typing.Never) 
-
-                return super().visit(node)
 
         if not options.use_typing_never:
             self._transform_types(NeverSayNeverT())
         else:
-            self._transform_types(NoReturnIsNowNeverT())
-
-        class ResolveMocksT(TypeInfo.Transformer):
-            """Resolves apparent test mock types to non-test ones."""
-            # TODO make mock resolution context sensitive, leaving test-only
-            # objects unresolved within test code?
-            def visit(vself, node: TypeInfo) -> TypeInfo:
-                node = super().visit(node)
-                if (resolved := resolve_mock(node, type_name_adjuster)):
-                    return resolved
-                return node
+            self._transform_types(NoReturnToNeverT())
 
         if options.resolve_mocks:
-            self._transform_types(ResolveMocksT())
-
-        class ExcludeTestTypesT(TypeInfo.Transformer):
-            """Removes test types."""
-            def visit(vself, node: TypeInfo) -> TypeInfo:
-                if is_test_module(node.module):
-                    return AnyTypeInfo
-
-                return super().visit(node)
+            self._transform_types(ResolveMocksT(type_name_adjuster))
 
         if options.exclude_test_types:
             self._transform_types(ExcludeTestTypesT())
 
-        class TypingUnionT(TypeInfo.Transformer):
-            """Replaces types.UnionType with typing.Union and typing.Optional."""
-            def visit(vself, node: TypeInfo) -> TypeInfo:
-                node = super().visit(node)
-
-                # Typevar nodes may be UnionType; there's no need to replace them, and
-                # replacing them would prevent RightTyper from annotating as typevars.
-                if node.type_obj is UnionType and not node.is_typevar():
-                    has_none = node.args[-1] == NoneTypeInfo
-                    non_none_count = len(node.args) - int(has_none)
-                    if non_none_count > 1:
-                        non_none = TypeInfo("typing", "Union", args=node.args[:non_none_count])
-                    else:
-                        assert isinstance(node.args[0], TypeInfo)
-                        non_none = node.args[0]
-
-                    if has_none:
-                        return TypeInfo("typing", "Optional", args=(non_none,))
-
-                    return non_none
-
-                return node
-
-        class DepthLimitT(TypeInfo.Transformer):
-            """Limits the depth of types (types within generic types)."""
-            def __init__(vself, limit: int):
-                vself._limit = limit
-                vself._level = -1
-                vself._maxLevel = -1
-
-            def visit(vself, node: TypeInfo) -> TypeInfo:
-                # Don't count lists (such as the arguments in a Callable) as a level,
-                # as it's not really a new type.
-                if node.is_list():
-                    return super().visit(node)
-
-                try:
-                    vself._level += 1
-                    vself._maxLevel = max(vself._maxLevel, vself._level)
-
-                    t = super().visit(node)
-
-                    if vself._maxLevel > vself._limit:
-                        # for containers, we can simply delete arguments (they default to Any)
-                        if (
-                            (type(t.type_obj) is type and issubclass(t.type_obj, abc.Container))
-                            or t.type_obj is abc.Callable
-                        ):
-                            vself._maxLevel = vself._level
-                            return t.replace(args=())
-
-                    return t
-                finally:
-                    vself._level -= 1
-
-        class MakePickleableTransformer(TypeInfo.Transformer):
-            """Clears code and type_obj on all TypeInfo: annotations are pickled by 'multiprocessing',
-               but these objects may not be pickleable.
-            """
-            def visit(vself, node: TypeInfo) -> TypeInfo:
-                if node.type_obj is not None:
-                    node = node.replace(type_obj=None)
-                if node.code is not None:
-                    node = node.replace(code=None)
-                return super().visit(node)
 
         finalizers: list[TypeInfo.Transformer] = []
-
-        if options.type_depth_limit is not None:
-            finalizers.append(DepthLimitT(options.type_depth_limit))
-
-        if options.use_typing_union:
-            finalizers.append(TypingUnionT())
-
-        finalizers.append(MakePickleableTransformer())
 
         def finalize(t: TypeInfo) -> TypeInfo:
             for f in finalizers:
                 t = f.visit(t)
             return t
+
+        if options.type_depth_limit is not None:
+            finalizers.append(DepthLimitT(options.type_depth_limit))
+
+        if options.use_typing_union:
+            finalizers.append(TypesUnionT())
+
+        finalizers.append(MakePickleableT())
 
         non_func_codes = self.variables.keys() - self.traces.keys()
 
